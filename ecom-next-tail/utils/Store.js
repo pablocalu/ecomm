7ad40c2a6dc86c50345925da0216ba5412552c20
Pablo Carro <pablo.carro@live.com>
2023-02-01 16:31:50 -0300

    order id

--- ecom-next-tail/utils/Store.js
+++ ecom-next-tail/utils/Store.js
@@ -36,6 +36,11 @@ function reducer(state, action){
                     paymentMethod: ''
                 }
             };
+        case 'CART_CLEAR_ITEMS':
+            return {
+                ...state,
+                cart: { ...state.cart, cartItems: []}
+            };
         case 'SAVE_SHIPPING_ADDRESS':
             return {
                 ...state,
